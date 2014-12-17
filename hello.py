c347d588538f168380e16b7e6401ee16e4ba60a0
Sebastian Cheung <sebastian_cheung@yahoo.com>
2014-12-17 18:49:13 +0000

    added flask-heroku package

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template
+from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
+heroku = Heroku(app)
 
 
 @app.route('/')
@@ -9,6 +11,6 @@ def hello_world():
     return render_template('index.html')
 
 
-
 if __name__ == '__main__':
     app.run(port=33507)
+    heroku.init_app(app)
