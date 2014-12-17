4aeb0540218fc624c15f9dec395aa13eac346999
Sebastian Cheung <sebastian_cheung@yahoo.com>
2014-12-17 19:34:25 +0000

    added blosc for bcolz dependency

---
+++ setup.py
@@ -0,0 +1,19 @@
+__author__ = 'minminsanjose'
+
+from setuptools import setup
+
+setup(
+    # cython --include-dir bcolz/ bcolz/carray_ext.pyx
+
+    # bcolz version:     0.7.2.dev
+    # bcolz git info:    v0.7.1-34-gfbe30e666d
+    # NumPy version:     1.9.0
+    # Blosc version:     1.4.1 ($Date:: 2014-07-08 #$)
+    # Blosc compressors: ['blosclz', 'lz4', 'lz4hc', 'snappy', 'zlib']
+    # Numexpr version:   2.3.1
+    # Python version:    2.7.8 |Anaconda 2.0.1 (64-bit)| (default, Aug 21 2014, 18:22:21)
+    # [GCC 4.4.7 20120313 (Red Hat 4.4.7-1)]
+    # Platform:          linux2-x86_64
+    # Byte-ordering:     little
+    # Detected cores:    4
+)
